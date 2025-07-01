93e0d3f8b31ed11f88e4d613eb464dab3ffb41ad
tttmb <annop.email@gmail.com>
2025-07-02 00:35:47 +0700

    first commit

---
+++ src/main/java/dev/annopud/prometheus_metrics/PrometheusMetricsApplication.java
@@ -0,0 +1,13 @@
+package dev.annopud.prometheus_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/annopud/prometheus_metrics/PrometheusMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.annopud.prometheus_metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
